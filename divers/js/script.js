c3df88539c7e9ced5a0072026709813dbef5f751
Makhnov <makh@tutanota.com>
2023-01-23 14:39:19 +0100

    lumiere ok

---
+++ divers/js/script.js
@@ -0,0 +1,6 @@
+// const interrupteur = document.getElementsByTagName('input')[0];
+
+// function onoff() {
+//     interrupteur.style.display = 'none';
+//     interrupteur.setAttribute('checked');
+// }
